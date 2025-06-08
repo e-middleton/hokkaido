d4163a6aff386a5b8084f631b2ee68373c494308
e-middleton <emiddleton@smith.edu>
2025-06-08 16:02:08 -0400

    minor changes to accommodate tokachi fault csv file

--- ishikari_fault.py
+++ ishikari_fault.py
@@ -14,8 +14,8 @@ from IPython.display import HTML
 
 
 # set up environment variables
-os.environ['CLAW'] = '/Users/anitamiddleton/Documents/python/clawpack/clawpack_src/clawpack'
-os.environ['FC'] = 'gfortran'
+# os.environ['CLAW'] = '/Users/emiddleton/Documents/python/clawpack/clawpack_src/clawpack'
+# os.environ['FC'] = 'gfortran'
 
 try:
     CLAW = os.environ['CLAW']
@@ -30,27 +30,47 @@ dir = os.path.join(CLAW, 'geoclaw/examples/hokkaido/')
 # Fault geometry is a 2D array with columns as described below
 # fault No., centroid(lon,lat,z[km]), node1(lon,lat,z[km]), node2(lon,lat,z[km]), 
 # node3(lon,lat,z[km]) , mean vertex length(km) , area(km^2) , strike(deg) , dip(deg)
-fault_geometry_file = os.path.join(dir + 'ishikari_fault.csv') 
-rupture_file = os.path.join(dir + 'ishikari_rupture.csv') #rupture information, currently only strike slip and dip slip for each subfault
+#fault_geometry_file = os.path.join(dir + 'ishikari_fault.csv') 
+fault_geometry_file = os.path.join(dir + 'tok_coords.csv')
+#rupture_file = os.path.join(dir + 'ishikari_rupture.csv') #rupture information, currently only strike slip and dip slip for each subfault
+rupture_file = os.path.join(dir + 'rupt_param.csv')
+
+# ishikari = np.loadtxt(fault_geometry_file, delimiter=",", skiprows=1) #path, comma separated values, first row is a header
+# ishikari[:,[3,6,9,12]] = 1e3*abs(ishikari[:,[3,6,9,12]]) #array slicing accesses depth element, changing it to be positive meters instead of negative km
+# rupture_parameters = np.loadtxt(rupture_file, delimiter=",")
+
+# fault0 = dtopotools.Fault() #create object
+# fault0.subfaults = [] #initialize empty list
+
+# nsubfaults = ishikari.shape[0]
+
+# # read in the data for each subfault
+# for j in range(nsubfaults):
+#     subfault0 = dtopotools.SubFault() #create new object
+#     node1 = ishikari[j,4:7].tolist() #lon,lat,depth of the first node in each triangle
+#     node2 = ishikari[j,7:10].tolist()
+#     node3 = ishikari[j,10:13].tolist()
+#     node_list = [node1,node2,node3]
+#     subfault0.slip = rupture_parameters[j,1] #all dip slip
+#     subfault0.rake = 90 #all the same
+#     subfault0.set_corners(node_list,projection_zone='10')
+#     fault0.subfaults.append(subfault0)
 
 ishikari = np.loadtxt(fault_geometry_file, delimiter=",", skiprows=1) #path, comma separated values, first row is a header
-ishikari[:,[3,6,9,12]] = 1e3*abs(ishikari[:,[3,6,9,12]]) #array slicing accesses depth element, changing it to be positive meters instead of negative km
-rupture_parameters = np.loadtxt(rupture_file, delimiter=",")
+# ishikari[:,[3,6,9,12]] = 1e3*abs(ishikari[:,[3,6,9,12]]) #array slicing accesses depth element, changing it to be positive meters
+ishikari[:,[2,5,8]] = 1e3*abs(ishikari[:,[2,5,8]]) #array slicing accesses depth element, changing it to be positive meters
 
 fault0 = dtopotools.Fault() #create object
 fault0.subfaults = [] #initialize empty list
 
 nsubfaults = ishikari.shape[0]
 
-# read in the data for each subfault
 for j in range(nsubfaults):
     subfault0 = dtopotools.SubFault() #create new object
-    node1 = ishikari[j,4:7].tolist() #lon,lat,depth of the first node in each triangle
-    node2 = ishikari[j,7:10].tolist()
-    node3 = ishikari[j,10:13].tolist()
+    node1 = ishikari[j,0:3].tolist() #lon,lat,depth of the first node in each triangle
+    node2 = ishikari[j,3:6].tolist()
+    node3 = ishikari[j,6:9].tolist()
     node_list = [node1,node2,node3]
-    subfault0.slip = rupture_parameters[j,1] #all dip slip
-    subfault0.rake = 90 #all the same
     subfault0.set_corners(node_list,projection_zone='10')
     fault0.subfaults.append(subfault0)
 
